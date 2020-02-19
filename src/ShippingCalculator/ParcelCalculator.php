30056d3368e8b0ebc65c83e4c112d6623365a91f
Ubuntu <ubuntu@ip-172-31-19-107.us-east-2.compute.internal>
2020-02-19 10:16:50 +0000

    Sylius ecommerce with assets

---
+++ src/ShippingCalculator/ParcelCalculator.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ShippingCalculator;
+
+use Sylius\Component\Shipping\Calculator\CalculatorInterface;
+use Sylius\Component\Shipping\Model\ShipmentInterface;
+
+final class ParcelCalculator implements CalculatorInterface
+{
+	public function calculate(ShipmentInterface $subject, array $configuration):int
+	{
+		$parcelSize=$configuration['size'];
+		$parcelPrice=$configuration['price'];
+
+		$numberOfPackages=ceil($subject->getUnits()->count()/$parcelSize);
+		return (int) ($numberOfPackages * $parcelPrice);
+	}
+
+	public function getType():string
+	{
+		return 'parcel';
+	}
+}
